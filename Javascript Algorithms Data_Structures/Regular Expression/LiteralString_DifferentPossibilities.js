8f877e2167c29ec01149acbe96db465302a1d54a
Niku1647 <patisubhakantaniku@gmail.com>
2020-08-04 21:58:32 +0530

    FCC

--- Javascript Algorithms Data_Structures/Regular Expression/LiteralString_DifferentPossibilities.js
+++ Javascript Algorithms Data_Structures/Regular Expression/LiteralString_DifferentPossibilities.js
@@ -1,4 +1,6 @@
-/* Complete the regex petRegex to match the pets "dog", "cat", "bird", or "fish". */
+/* 
+* Complete the regex petRegex to match the pets "dog", "cat", "bird", or "fish". 
+*/
 
 let petString = "James has a pet cat.";
 let petRegex = /dog|cat|bird|fish/; // Change this line
--- Javascript Algorithms Data_Structures/Regular Expression/MatchLiteral.js
+++ Javascript Algorithms Data_Structures/Regular Expression/MatchLiteral.js
@@ -1,4 +1,7 @@
-/* Complete the regex waldoRegex to find "Waldo" in the string waldoIsHiding with a literal match. */
+/* 
+  Complete the regex waldoRegex to find "Waldo" 
+  in the string waldoIsHiding with a literal match. 
+*/
 
 let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
 let waldoRegex = /Waldo/; // Change this line
