0e809b5135ffdb3e1ef4171842862978c00d6a22
Niku1647 <patisubhakantaniku@gmail.com>
2020-08-12 09:36:52 +0530

    FCC

---
+++ Javascript Algorithms Data_Structures/Functional/4_ Avoid Mutations&SideEffects.js
@@ -0,0 +1,18 @@
+/*
+
+Fill in the code for the function incrementer 
+so it returns the value of the global variable fixedValue increased by one.
+
+*/
+
+// The global variable
+var fixedValue = 4;
+
+function incrementer () {
+  // Only change code below this line
+  var lastValue = fixedValue
+  return lastValue+1
+
+
+  // Only change code above this line
+}
