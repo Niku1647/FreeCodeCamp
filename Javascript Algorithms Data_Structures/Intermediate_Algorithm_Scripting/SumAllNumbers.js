286dcda780f45a35e65c8ab362aef2fe329ce2c6
Niku1647 <patisubhakantaniku@gmail.com>
2020-09-08 12:29:21 +0530

    FCC

---
+++ Javascript Algorithms Data_Structures/Intermediate_Algorithm_Scripting/SumAllNumbers.js
@@ -0,0 +1,26 @@
+/*
+
+We'll pass you an array of two numbers. 
+Return the sum of those two numbers plus the sum of all the numbers between them. 
+The lowest number will not always come first.
+
+For example, sumAll([4,1]) should return 10 because sum of all the numbers between 1 
+and 4 (both inclusive) is 10.
+
+
+*/
+
+
+function sumAll(arr) {
+    var j= Math.max(arr[0],arr[1])
+    var i= Math.min(arr[0],arr[1])
+    var sum = 0
+   for(i;i<=j;i++){
+     sum += i
+   }
+  
+    return sum;
+  }
+  
+  sumAll([1, 4]);
+  
