5b7fec78615ac5861607dc5f629f54e08b371f42
Subhakanta Pati <59618850+Niku1647@users.noreply.github.com>
2020-12-28 21:18:16 +0530

    Update 14_concatInsteadofpush.js

--- Javascript Algorithms Data_Structures/Functional/14_concatInsteadofpush.js
+++ Javascript Algorithms Data_Structures/Functional/14_concatInsteadofpush.js
@@ -8,10 +8,10 @@ The function should return an array.
 */
 
 function nonMutatingPush(original, newItem) {
-    // Only change code below this line
+   
     return original.concat(newItem);
   
-    // Only change code above this line
+    
   }
   var first = [1, 2, 3];
   var second = [4, 5];
